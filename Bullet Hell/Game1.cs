aa634511ff19fc6048ef0c67e1a5ee583dfafc43
linne01lampa <linus.bensryd@gmail.com>
2018-05-08 09:38:52 +0200

    Ändrade kanppar för att pause och stänga av

--- Bullet Hell/Game1.cs
+++ Bullet Hell/Game1.cs
@@ -120,7 +120,7 @@ namespace Bullet_Hell
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.F4))
                 Exit();
 
             // TODO: Add your update logic here
